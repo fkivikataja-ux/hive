6a30519a51e314c3812609cd5b6f841f7d49fa7c
fanikivikataja <fkivikataja@gmail.com>
2026-08-27 11:52:18 +0300

    Update mean.go

--- mean.go
+++ mean.go
@@ -4,3 +4,35 @@ func Mean(a, b, c float32) float32 {
    // code to be executed 
      return (a+b+c) / 3.0
 }
+
+
+/*
+Mandatory 1 / 1
+1
+Mean
+
+Learning Objectives:
+
+    Understanding the structure of functions
+    Ability to work with values stored in the variables
+    Understanding float data types and how to perform arithmetic operations on them
+
+Instructions
+
+Write a function that takes three float32 values as input and returns their mean as a float32 value.
+Expected function signature
+
+package sprint
+
+func Mean(a, b, c float32) float32 {
+
+}
+
+Usage
+
+Mean(1.25, -2.0, 9.75)
+>> 3.0
+
+
+
+*/
