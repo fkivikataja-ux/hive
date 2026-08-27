e335b42963c55816a4c6283f38fb3c2d397ebca1
fanikivikataja <fkivikataja@gmail.com>
2026-08-27 11:52:46 +0300

    Update casting.go

--- casting.go
+++ casting.go
@@ -6,3 +6,35 @@ func Casting(n float64) int {
 	return int(math.Round (n))
 }
 
+/*
+Casting
+
+Learning Objectives:
+
+    Gain proficiency in type casting by successfully converting a float64 to an int, ensuring that fractional parts are rounded appropriately.
+    Develop an understanding of rounding techniques, including rounding to the nearest integer, and implement this logic accurately within the function.
+
+Instructions
+
+Write a function that accepts a float64 value, rounds it to the nearest integer, and then returns the result as an int.
+Allowed packages
+
+For this task you're allowed to use:
+
+    math
+
+Expected function signature
+
+package sprint
+
+func Casting(n float64) int {
+
+}
+
+Usage
+
+Casting(1.15)
+>> 1
+
+
+*/
