aa6a28e08d657cb706167295f51750b83dfc1943
fanikivikataja <fkivikataja@gmail.com>
2026-08-27 11:50:07 +0300

    Update abacus.go

--- abacus.go
+++ abacus.go
@@ -2,4 +2,38 @@ package sprint
 func Abacus(a int, b int) int {
  return a / b
 }
+/*
+Uppgift: Abacus
 
+Objective: Dive into the Go language by understanding the structure of functions, managing variables, returning values, using arithmetic operators, and more.
+
+Learning Objectives:
+
+    Understand the structure of functions in Go.
+    Manage and manipulate values stored in variables.
+    Use the return statement effectively.
+    Test and debug your code for any issues.
+
+Instructions
+
+Create a function named Abacus that takes two integer (int) values as parameters: a, the dividend, and b, the divisor. The function should return the result of integer division.
+Expected function signature
+
+package sprint
+
+func Abacus(a int, b int) int {
+
+}
+
+Usage
+
+Abacus(8, 3)
+>> 2
+
+Abacus(9, 2)
+
+
+
+
+
+*/
